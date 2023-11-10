b99a17aa8832a7a16075c1327172d1cd69f78991
Nik Tomazic <nick.tomazic@gmail.com>
2023-11-10 14:15:46 +0000

    installed and configured appsignal

---
+++ __appsignal__.py
@@ -0,0 +1,10 @@
+from appsignal import Appsignal
+
+appsignal = Appsignal(
+    active=True,
+    name="django-error-tracking",
+    # Please do not commit this key to your source control management system.
+    # Move this to your app's security credentials or environment variables.
+    # https://docs.appsignal.com/python/configuration/options.html#option-push_api_key
+    push_api_key="<your_push_api_key>",
+)
