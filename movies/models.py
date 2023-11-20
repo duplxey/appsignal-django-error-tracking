88a61ed81aa5506dd35dbe517d744611a5151e62
Nik Tomazic <nick.tomazic@gmail.com>
2023-11-20 20:19:24 +0000

    dropped djangorestframework, simplified code, fixed Movie.json fixture

--- movies/models.py
+++ movies/models.py
@@ -1,4 +1,3 @@
-from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
 
@@ -15,22 +14,34 @@ class Movie(models.Model):
 
         return sum(review.rating for review in reviews) / len(reviews)
 
+    def serialize_to_json(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'description': self.description,
+            'release_year': self.release_year,
+        }
+
     def __str__(self):
         return f'{self.title} ({self.release_year})'
 
 
 class MovieReview(models.Model):
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
-    rating = models.IntegerField(validators=[
-        MinValueValidator(1),
-        MaxValueValidator(5),
-    ])
+    rating = models.IntegerField()
 
     def save(self, force_insert=False, force_update=False, using=None, update=None):
         if self.rating < 1 or self.rating > 5:
             raise ValueError('Rating must be between 1 and 5.')
 
         super().save(force_insert, force_update, using, update)
+
+    def serialize_to_json(self):
+        return {
+            'id': self.id,
+            'movie': self.movie.serialize_to_json(),
+            'rating': self.rating,
+        }
 
     def __str__(self):
         return f'{self.movie.title} - {self.rating}'
--- movies/views.py
+++ movies/views.py
@@ -1,34 +1,62 @@
-from rest_framework import viewsets, status
-from rest_framework.decorators import action
-from rest_framework.response import Response
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_http_methods
 
-from movies.models import Movie
-from movies.serializers import MovieSerializer, MovieReviewSerializer
+from movies.models import Movie, MovieReview
 
 
-class MovieViewSet(viewsets.ModelViewSet):
+def index_view(request):
     queryset = Movie.objects.all()
-    serializer_class = MovieSerializer
+    data = [movie.serialize_to_json() for movie in queryset]
 
-    @action(detail=False, methods=['get'])
-    def statistics(self, request, pk=None):
-        data = []
+    return JsonResponse(data, safe=False)
 
-        for movie in Movie.objects.all():
-            data.append({
-                'id': movie.id,
-                'title': movie.title,
-                'average_rating': movie.get_average_rating(),
-            })
 
-        return Response(data)
+def statistics_view(request):
+    queryset = Movie.objects.all()
+    data = []
 
-    @action(detail=True, methods=['post'])
-    def review(self, request, pk=None):
-        serializer = MovieReviewSerializer(data=request.data)
+    for movie in queryset:
+        data.append({
+            'id': movie.id,
+            'title': movie.title,
+            'average_rating': movie.get_average_rating(),
+        })
 
-        if serializer.is_valid():
-            serializer.save()
-            return Response({'success': 'Review created.'}, status=201)
+    return JsonResponse(data, safe=False)
 
-        return Response(serializer.errors, status=400)
+
+@csrf_exempt
+@require_http_methods(["POST"])
+def review_view(request):
+    movie_id = request.POST.get('movie')
+    rating = request.POST.get('rating')
+
+    if (movie_id is None or rating is None) or (not movie_id.isdigit() or not rating.isdigit()):
+        return JsonResponse({
+            'detail': 'Please provide a `movie` (int) and `rating` (int).',
+        }, status=400)
+
+    movie_id = int(movie_id)
+    rating = int(rating)
+
+    if rating < 1 or rating > 5:
+        return JsonResponse({
+            'detail': 'Rating must be between 1 and 5.',
+        }, status=400)
+
+    try:
+        movie = Movie.objects.get(id=movie_id)
+        MovieReview.objects.create(
+            movie=movie,
+            rating=rating,
+        )
+
+        return JsonResponse({
+            'detail': 'A review has been successfully posted',
+        })
+
+    except Movie.DoesNotExist:
+        return JsonResponse({
+            'detail': 'Movie does not exist.',
+        }, status=400)
